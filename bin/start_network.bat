c80a138249c7e8c7085945a3710d96aa31b65efc
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-28 11:57:50 +0200

    Code regeneration/Model synchronization

--- bin/start_network.bat
+++ bin/start_network.bat
@@ -7,5 +7,5 @@ cd ..
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*;"
 
-java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -w -p 9011 uploadStartupDirectory('etc/startup') startService('i5.las2peer.services.votes.votes@0.1') startWebConnector interactive
+java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -w -p 9011 uploadStartupDirectory('etc/startup') startService('i5.las2peer.services.votes.votes@0.2') startWebConnector interactive
 pause
