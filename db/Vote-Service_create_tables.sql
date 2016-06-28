7bfb69bcffc3abe409a6acc3819a4cc169aabf27
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-28 11:24:56 +0200

    Generated microservice version 0.1

---
+++ db/Vote-Service_create_tables.sql
@@ -0,0 +1,20 @@
+--
+-- Database Schema:  schema1 
+-- Automatically generated sql script for the service Vote-Service, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table user.
+--
+CREATE TABLE schema1.user (
+  userId int ,
+CONSTRAINT userId_PK PRIMARY KEY (userId)
+);
+--
+-- Table structure for table Table1.
+--
+CREATE TABLE schema1.Table1 (
+  id int ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+);
+
