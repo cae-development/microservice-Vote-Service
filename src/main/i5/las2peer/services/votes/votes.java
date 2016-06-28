141cf3f0147afbfa92f34cd5813bc70ca7485bd1
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-28 11:28:52 +0200

    Http Response[numbers] edited by Thomas Winkler

--- src/main/i5/las2peer/services/votes/votes.java
+++ src/main/i5/las2peer/services/votes/votes.java
@@ -107,9 +107,27 @@ public class votes extends Service {
     // numbers
     boolean numbers_condition = true;
     if(numbers_condition) {
-      JSONObject resultJson = new JSONObject();
-      HttpResponse numbers = new HttpResponse(resultJson.toJSONString(), HttpURLConnection.HTTP_CREATED);
-      return numbers;
+JSONArray array = new JSONArray();
+      Connection conn = null;
+      try {
+        conn = dbm.getConnection();
+        PreparedStatement statement = conn.prepareStatement("Insert into test (number) Values (1);");
+        statement.executeUpdate();
+        statement = conn.prepareStatement("Select * from test");
+        ResultSet result = statement.executeQuery();
+        while (result.next()) {
+          array.add(result.getInt("number"));
+        }
+        resultJson.put("numbers", array);
+        conn.close();
+
+        HttpResponse testResult = new HttpResponse(resultJson.toJSONString(), HttpURLConnection.HTTP_OK);
+        return testResult;
+      } catch (Exception e) {
+        e.printStackTrace();
+        HttpResponse testResult = new HttpResponse("Internal Error", HttpURLConnection.HTTP_INTERNAL_ERROR);
+        return testResult;
+      }
     }
     return null;
   }
