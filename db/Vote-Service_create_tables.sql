6076f4549cb811c6a264a0a9d7e1c37e25913a73
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-28 11:26:09 +0200

    Code regeneration/Model synchronization

--- db/Vote-Service_create_tables.sql
+++ db/Vote-Service_create_tables.sql
@@ -1,19 +1,19 @@
 --
--- Database Schema:  schema1 
+-- Database Schema:  test 
 -- Automatically generated sql script for the service Vote-Service, created by the CAE.
 -- --------------------------------------------------------
 
 --
 -- Table structure for table user.
 --
-CREATE TABLE schema1.user (
+CREATE TABLE test.user (
   userId int ,
 CONSTRAINT userId_PK PRIMARY KEY (userId)
 );
 --
 -- Table structure for table Table1.
 --
-CREATE TABLE schema1.Table1 (
+CREATE TABLE test.Table1 (
   id int ,
 CONSTRAINT id_PK PRIMARY KEY (id)
 );
